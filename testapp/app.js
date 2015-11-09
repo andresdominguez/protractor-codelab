d8891d3714e84d16fa11449eae3212f56c3a0380
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-11-08 19:31:16 -0500

    Add exercise 1 in ng app

--- testapp/app.js
+++ testapp/app.js
@@ -4,7 +4,48 @@
     'ngRoute'
   ]);
 
-  app.controller('Exercise1Ctrl', function() {
-    this.name = 'hello';
-  })
+  app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'main.html'
+    }).when('/exercise1', {
+      templateUrl: 'exercise1.html',
+      controller: 'Exercise1Ctrl',
+      controllerAs: 'ctrl'
+    }).when('/exercise2', {
+      templateUrl: 'exercise2.html',
+      controller: 'Exercise2Ctrl',
+      controllerAs: 'ctrl'
+    }).when('/exercise3', {
+      templateUrl: 'exercise3.html',
+      controller: 'Exercise3Ctrl',
+      controllerAs: 'ctrl'
+    }).when('/addUser', {
+      templateUrl: 'addUser.html',
+      controller: 'AddUserCtrl',
+      controllerAs: 'ctrl'
+    }).otherwise({
+      redirectTo: '/'
+    });
+  });
+
+  app.service('userService', function($resource) {
+    return {
+      users: $resource('/users')
+    };
+  });
+
+  app.controller('Exercise1Ctrl', function(userService) {
+    var vm = this;
+
+    vm.user = {
+      name: 'Bruce Lee',
+      email: 'bruce.lee@google.com',
+      phone: '212 555-1234'
+    };
+
+    vm.updateData = function() {
+      vm.displayUser = angular.copy(vm.user);
+    };
+    vm.updateData();
+  });
 })();
