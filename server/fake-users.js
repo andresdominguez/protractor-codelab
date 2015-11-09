9f30d696382945f22e2bfbf97e09b326c6e2a028
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-11-08 23:18:53 -0500

    Merge master changes

--- server/fake-users.js
+++ server/fake-users.js
@@ -10,13 +10,34 @@ var _ = require('lodash');
  * }>}
  */
 var getUsers = function() {
-  return _.times(3).map(function() {
+  var chuckNorris = {
+    name: 'Chuck Norris',
+    email: 'chuck@gmail.com',
+    phone: '212-555-1234'
+  };
+  var rambo = {
+    name: 'John Rambo',
+    email: 'rambo@gmail.com',
+    phone: '415-555-9876'
+  };
+
+  var fakeUsers = _.times(6).map(function() {
     return {
       name: faker.name.findName(),
       email: faker.internet.email(),
       phone: faker.phone.phoneNumber()
-    }
-  })
+    };
+  });
+
+  // Add chuck norris at the beginning
+  fakeUsers.unshift(chuckNorris);
+
+  // Add rambo in the middle (range 1-4)
+  var randomIndex = (new Date()).getTime() % 4 + 1;
+  // removes 0 elements from index 'randomIndex', and inserts item
+  fakeUsers.splice(randomIndex, 0, rambo);
+
+  return fakeUsers;
 };
 
 module.exports = {
