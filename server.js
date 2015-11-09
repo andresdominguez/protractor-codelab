727c71058ff0f40d0bb38d6d931687e62a14f2d4
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-11-08 21:09:09 -0500

    Add persistent user

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
+var bodyParser = require('body-parser');
 var express = require('express');
-var app = express();
 var fakeUsers = require('./server/fake-users');
 
-var TIMEOUT = 1000;
+var app = express();
+
+// Serve all the angular files under testapp
+app.use(
+    express.static(__dirname + '/testapp'),
+    express.static(__dirname));
+app.use(bodyParser.json());
 
 var start = function() {
-  // Serve all the angular files under testapp
-  app.use(
-      express.static(__dirname + '/testapp'),
-      express.static(__dirname));
+
+  var persistentUsers = fakeUsers.getUsers();
 
   app.get('/users', function(req, res) {
     res.send({list: fakeUsers.getUsers()});
+  });
+
+  app.get('/persistent/list', function(req, res) {
+    res.send({list: persistentUsers});
+  });
+
+  app.get('/persistent/list', function(req, res) {
+    res.send({list: persistentUsers});
+  });
+
+  app.post('/persistent/add', function(req, res) {
+    if (!req.body || !req.body.user) {
+      res.sendStatus(400);
+    }
+    persistentUsers.push(req.body.user);
+    res.sendStatus(200);
   });
 
   var server = app.listen(9000, function() {
