4299abdf9ddc60a9c2b2455099dc18e11f90fc5f
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-11-15 12:23:52 -0500

    Add empty exercise 3

---
+++ spec/ex3-spec.js
@@ -0,0 +1,11 @@
+describe('UExercise 3', function() {
+  it('should add a new user using page objects', function() {
+    // 1. Create a page object for the exercise 3 list view
+    // 2. Create a page object for the exercise 3 new user view
+    // 3. The test should do the following:
+    //   Navigate to exercise 3 list
+    //   CLick on the "Add user" button
+    //   Enter data for a new user and click "Save"
+    //   Ensure the new user was added to the list
+  });
+});
